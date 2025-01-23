2950abff69c478e4bf1b5c4fa5285ce23fb2f3ee
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2025-01-23 14:03:14 +0200

    commiting the source folder

---
+++ src/main/java/com/surreal/StockMarketAdviser/StockMarketAdviserApplication.java
@@ -0,0 +1,13 @@
+package com.surreal.StockMarketAdviser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketAdviserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMarketAdviserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surreal/StockMarketAdviser/StockMarketAdviserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surreal.StockMarketAdviser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketAdviserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
